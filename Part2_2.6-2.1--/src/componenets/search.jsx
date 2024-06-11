92c002d4a16cc89960e9efd30107941c62123175
santiago10181 <92320879+santiago10181@users.noreply.github.com>
2024-06-11 22:28:07 +0000

    fixed 2.6 to 2.10

--- Part2_2.6-2.1--/src/componenets/search.jsx
+++
@@ -1,9 +0,0 @@
-const SearchFilter = ({ filtroBusqueda, onChange }) => {
-    return (
-      <div>
-        <input type="text" placeholder="Buscar" value={filtroBusqueda} onChange={onChange} />
-      </div>
-    );
-  };
-
-  export default SearchFilter
